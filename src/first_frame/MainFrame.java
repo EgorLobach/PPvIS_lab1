b3766ac713b699fcbb10dac39be7eb7c46b81b22
EgorLobach <mega.lobach@gmail.com>
2017-02-28 18:11:33 +0300

    second commit

--- src/first_frame/MainFrame.java
+++ src/first_frame/MainFrame.java
@@ -120,10 +120,7 @@ public class MainFrame extends JFrame {
 
         //task3end***********************************************
 
-        //task4
 
-
-        //task4end****************************************************
 
         setVisible(true);
         pack();
